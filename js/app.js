a062885bc52c3df231d9212114269bb0f85ea56c
jonathan-crepeau <jon.crepeau@gmail.com>
2022-05-30 19:31:45 -0700

    reference questions completed

--- js/app.js
+++ js/app.js
@@ -1,2 +1,33 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
 
+// const select = document.querySelector('.select');
+// const inner = select.querySelectorAll(':scope .outer ,.inner');
+// console.log(inner);
+
+// const match = $('div').eq(2);
+// console.log(match);
+// console.log(match.length)
+
+// const match = document.getElementById('sentence').innerHTML;
+// console.log(match);
+
+// const paragraphs = document.querySelectorAll('p');
+// console.log(paragraphs);
+// console.log(paragraphs[0].nodeName);
+
+// const match = $('p');
+// console.log(match);
+// console.log(match[0].nodeName);
+
+// window.onload = function() {
+//   const heading = document.createElement('h1');
+//   const heading_text = document.createTextNode('Big Header!');
+//   heading.appendChild(heading_text);
+//   document.body.appendChild(heading);
+// }
+
+// const pElement = $('#sentence').ownerDocument;
+// console.log(pElement);
+
+// const test = document.getElementById('sentence').nodeType;
+// console.log(test);
